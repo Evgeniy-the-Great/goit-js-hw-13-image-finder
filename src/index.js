42869118af11fa9f82bf838a3852b11a884fd645
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-08 22:02:30 +0300

    add gallery

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-import './sass/main.scss';
+import refs from './js/Refs';
+import cardMarkup from './tamplates/cardMarcup.hbs';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
+
+let page = 1;
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const value = e.currentTarget.elements.query.value;
+  const BASE_URL = 'https://pixabay.com/api/';
+  const queryParam = new URLSearchParams({
+    key: '23292675-06f406722274daa99671b1028',
+    image_type: 'photo',
+    q: value,
+    orientation: 'horizontal',
+    // page: 1,
+    per_page: 12,
+  });
+
+  fetch(`${BASE_URL}?${queryParam}&page=${page}`)
+    .then(res => res.json())
+    .then(data => {
+      renderCard(data);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    });
+
+  function renderCard({ hits }) {
+    refs.list.innerHTML = cardMarkup(hits);
+  }
+}
+
+function incrementPage () {
+  page += 1;
+};
+
+console.log(refs.form);
+
+function onLoadMoreBtnClick() {
+  incrementPage();
+  const BASE_URL = 'https://pixabay.com/api/';
+  const queryParam = new URLSearchParams({
+    key: '23292675-06f406722274daa99671b1028',
+    image_type: 'photo',
+    q: refs.form.elements.query.value,
+    orientation: 'horizontal',
+    page: 1,
+    per_page: 12,
+  });
+
+  fetch(`${BASE_URL}?${queryParam}&page=${page}`)
+    .then(res => res.json())
+    .then(data => {
+      renderCard(data);
+      refs.loadMoreBtn.classList.remove('is-hidden');
+    });
+
+  function renderCard({ hits }) {
+    const markup = cardMarkup(hits);
+    refs.list.insertAdjacentHTML('beforeend', markup);
+      
+  }
+}
---
+++ src/js/Refs.js
@@ -0,0 +1,7 @@
+export default {
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('.input-form'),
+  formBtn: document.querySelector('.js-form-btn'),
+  loadMoreBtn: document.querySelector('.js-load-btn'),
+  list: document.querySelector('.gallery'),
+};
